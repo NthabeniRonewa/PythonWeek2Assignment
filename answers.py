d9ca24e102e53f7d1f48a4b0da58a8995c846d5a
Nthabeni Ronewa <nthabenironewa@gmail.com>
2024-11-06 10:41:09 +0200

    Week 2

---
+++ answers.py
@@ -0,0 +1,23 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1 , 15)
+
+list_extend =[50,60,70]
+
+my_list.extend(list_extend)
+
+my_list.remove(70)
+
+my_list.sort()
+
+print(my_list.index(30))
+
+print(my_list)
+
+
+ 
